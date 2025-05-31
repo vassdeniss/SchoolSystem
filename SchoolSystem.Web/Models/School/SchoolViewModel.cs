2b31c5b032464e5e267845444ed12876323b7aec
Denis Vasilev <vassdeniss@gmail.com>
2025-05-31 18:13:16 +0300

    feat: added styled school index

--- SchoolSystem.Web/Models/School/SchoolViewModel.cs
+++ SchoolSystem.Web/Models/School/SchoolViewModel.cs
@@ -1,8 +1,4 @@
-﻿using SchoolSystem.Infrastructure.Models;
-using SchoolSystem.Web.Models.Class;
-using SchoolSystem.Web.Models.Subject;
-
-namespace SchoolSystem.Web.Models.School;
+﻿namespace SchoolSystem.Web.Models.School;
 
 public class SchoolViewModel
 {
@@ -13,10 +9,4 @@ public class SchoolViewModel
     public string Address { get; init; } = null!;
     
     public string? PrincipalName  { get; init; }
-        
-    //public ICollection<TeacherViewModel> Teachers { get; set; } = new HashSet<Teacher>();
-    
-    public ICollection<ClassViewModel> Classes { get; set; } = new HashSet<ClassViewModel>();
-    
-    public ICollection<SubjectViewModel> Subjects { get; set; } = new HashSet<SubjectViewModel>();
 }
